3f630439a5e3f77986396900c92e4a341eda4057
Stanley Liu <stanleyliu@Stanleys-MacBook-Pro.local>
2017-06-08 14:31:22 +0800

    Added GIN view controllers

---
+++ GIN/GINKeynoteViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GINKeynoteViewController.swift
+//  CISSMUN
+//
+//  Created by Stanley Liu on 6/8/17.
+//  Copyright © 2017 Stanley Liu. All rights reserved.
+//
+
+import UIKit
+
+class GINKeynoteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- GIN/MUNScheduleViewController.swift
+++ GIN/MUNScheduleViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class MUNScheduleViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
