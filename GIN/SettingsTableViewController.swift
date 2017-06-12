74fc342aba449c38b905543ba529b8ef07e1531b
Stanley Liu <stanleyliu@Stanleys-MacBook-Pro.local>
2017-06-12 16:44:09 +0800

    Designed settings menu

--- GIN/SettingsTableViewController.swift
+++ GIN/SettingsTableViewController.swift
@@ -29,7 +29,7 @@ class SettingsTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 1
+        return 2
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
