ebd2ca1285d886c53e1dbbc18b78098c599a0ba4
Stanley Liu <stanleyliu@Stanleys-MacBook-Pro.local>
2017-06-12 21:42:38 +0800

    Fixed Google.plist file

--- GIN/AppDelegate.swift
+++ GIN/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        FirebaseApp.configure()
+        
         // Setting appearances/formatting for navigation bar
         let attrs = [
             NSForegroundColorAttributeName: UIColor.white,
