22709c01f7cf21e7b837802595e6d8d2ca0a2995
rolandrao <rolandrao@gmail.com>
2017-07-10 20:20:08 -0400

    Shanghai View back button

---
+++ GIN/ShanghaiViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ShanghaiViewController.swift
+//  CISSMUN
+//
+//  Created by Roland on 7/10/17.
+//  Copyright © 2017 Stanley Liu. All rights reserved.
+//
+
+import UIKit
+
+class ShanghaiViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func backButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
