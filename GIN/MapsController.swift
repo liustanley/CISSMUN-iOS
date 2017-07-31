129a1b93c6204f8566d1b764c0b79a9600fb93f4
Edward Huang <edwardhuang@Edwards-MacBook-Pro.local>
2017-07-31 04:14:02 -0700

    fixes made

---
+++ GIN/MapsController.swift
@@ -0,0 +1,38 @@
+//
+//  MapsController.swift
+//  CISSMUN
+//
+//  Created by Edward Huang on 31/7/2017.
+//  Copyright © 2017 Stanley Liu. All rights reserved.
+//
+
+import UIKit
+
+class MapsController: UIViewController {
+    
+    @IBOutlet weak var menuConstraint: NSLayoutConstraint!
+    
+    var menuShowing = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+  
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func menuOpen(_ sender: Any) {
+        
+        if(menuShowing) {
+            menuConstraint.constant = -145
+        } else {
+            menuConstraint.constant = 0
+        }
+        menuShowing = !menuShowing
+    }
+    
+}
