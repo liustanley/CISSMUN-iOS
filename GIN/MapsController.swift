59f99d5277c66dbaa0bdff1797cc1d68e4cf21fd
Edward Huang <edwardhuang@Edwards-MacBook-Pro.local>
2017-09-05 22:03:27 +0800

    code changed.

--- GIN/MapsController.swift
+++ GIN/MapsController.swift
@@ -11,19 +11,29 @@ import UIKit
 class MapsController: UIViewController {
     
 
+    @IBOutlet weak var leadingConstraint: NSLayoutConstraint!
+    
+    var menuShowing = false;
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+      
   
        
         
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func openMenu(_ sender: Any) {
+        
+        if(menuShowing){
+        leadingConstraint.constant = -200;
+        }
+        else {
+        leadingConstraint.constant = 0;
+        }
+        
+        menuShowing = !menuShowing;
+        
     }
 
-    
 }
