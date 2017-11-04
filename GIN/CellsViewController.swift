c4f90193f5b3ae37e855d66022a493f5e00ecdf9
Edward Huang <edwardhuang@Edwards-MacBook-Pro.local>
2017-11-05 02:00:50 +0800

    Cells available for all search tableViews!

---
+++ GIN/CellsViewController.swift
@@ -0,0 +1,28 @@
+//
+//  CellsViewController.swift
+//  CISSMUN
+//
+//  Created by Edward Huang on 5/11/2017.
+//  Copyright © 2017 Stanley Liu. All rights reserved.
+//
+
+import UIKit
+
+class CellsViewController: UIViewController {
+
+    @IBOutlet weak var textView: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
--- GIN/LocationSearch.swift
+++ GIN/LocationSearch.swift
@@ -8,11 +8,15 @@
 
 import UIKit
 
+var array = ["H015A, PE Commons", "H115, HS Gym", "P228, Phoenix Café", "PC Lounge", "H227, HS Library", "H233, HS Conference Room", "H316/H318, Wittenberg Hall", "H329, Business Hub Conference Room", "H405/H406, Augsburg Hall", "P106, PC Gym", "M116, MS Gym", "E111, ES Motor Skills Room", "E322, ES Library", "E122, ES Cafeteria", "R302, Orchestra Room", "PC Commons", "H116, Old Luther", "P230/P231, Jade/Pearl", "P202", "R110, Drama Room", "H117, Makerspace", "H127, Health Room", "R101, Rittman Theatre", "Room #, Costume Room", "MSR?????", "H404", "H403", "P201, New Luther", "H305", "H205", "P106, Phoenix Center Gym", "Classroom next to Jade/Pearl???", "H203", "H204", "H206", "MS and HS classrooms Schools???"
+]
+
+var row = 0
+
 class LocationSearch: UITableViewController, UISearchResultsUpdating{
 
     
-    var array = ["H015A, PE Commons", "H115, HS Gym", "P228, Phoenix Café", "PC Lounge", "H227, HS Library", "H233, HS Conference Room", "H316/H318, Wittenberg Hall", "H329, Business Hub Conference Room", "H405/H406, Augsburg Hall", "P106, PC Gym", "M116, MS Gym", "E111, ES Motor Skills Room", "E322, ES Library", "E122, ES Cafeteria", "R302, Orchestra Room", "PC Commons", "H116, Old Luther", "P230/P231, Jade/Pearl", "P202", "R110, Drama Room", "H117, Makerspace", "H127, Health Room", "R101, Rittman Theatre", "Room #, Costume Room", "MSR?????", "H404", "H403", "P201, New Luther", "H305", "H205", "P106, Phoenix Center Gym", "Classroom next to Jade/Pearl???", "H203", "H204", "H206", "MS and HS classrooms Schools???"
-    ]
+    
     
     var filtered = [String]()
     var searchController : UISearchController!
@@ -49,9 +53,6 @@ class LocationSearch: UITableViewController, UISearchResultsUpdating{
     
     
     // MARK: - Table view data source
-    
-    
-    
         override  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 
         if tableView == resultsController.tableView{
@@ -64,7 +65,9 @@ class LocationSearch: UITableViewController, UISearchResultsUpdating{
     }
     
         override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
+            
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")! as UITableViewCell
+            
     
         if tableView == resultsController.tableView{
             cell.textLabel?.text = filtered[indexPath.row]
@@ -73,6 +76,10 @@ class LocationSearch: UITableViewController, UISearchResultsUpdating{
             cell.textLabel?.text = array[indexPath.row]
         }
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        row = indexPath.row
     }
     /*
     func tableView(_ tableView: UITableView, didSelectRow indexPath: IndexPath) {
