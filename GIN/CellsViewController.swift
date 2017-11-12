b40b36ba3af2dbd843f9b79027d35972d4949ead
Edward Huang <edwardhuang@Edwards-MacBook-Pro.local>
2017-11-12 14:48:17 +0800

    two Images added to search function

--- GIN/CellsViewController.swift
+++ GIN/CellsViewController.swift
@@ -10,11 +10,14 @@ import UIKit
 
 class CellsViewController: UIViewController {
 
-    @IBOutlet weak var textView: UILabel!
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        imageView.image = images[row]
         // Do any additional setup after loading the view.
     }
 
--- GIN/LocationSearch.swift
+++ GIN/LocationSearch.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-var array = ["H015A, PE Commons", "H115, HS Gym", "P228, Phoenix Café", "PC Lounge", "H227, HS Library", "H233, HS Conference Room", "H316/H318, Wittenberg Hall", "H329, Business Hub Conference Room", "H405/H406, Augsburg Hall", "P106, PC Gym", "M116, MS Gym", "E111, ES Motor Skills Room", "E322, ES Library", "E122, ES Cafeteria", "R302, Orchestra Room", "PC Commons", "H116, Old Luther", "P230/P231, Jade/Pearl", "P202", "R110, Drama Room", "H117, Makerspace", "H127, Health Room", "R101, Rittman Theatre", "Room #, Costume Room", "MSR?????", "H404", "H403", "P201, New Luther", "H305", "H205", "P106, Phoenix Center Gym", "Classroom next to Jade/Pearl???", "H203", "H204", "H206", "MS and HS classrooms Schools???"
+var array = ["H316/H318, Wittenberg Hall", "PC Lounge","H015A, PE Commons", "H115, HS Gym", "P228, Phoenix Café",  "H227, HS Library", "H233, HS Conference Room", "H329, Business Hub Conference Room", "H405/H406, Augsburg Hall", "P106, PC Gym", "M116, MS Gym", "E111, ES Motor Skills Room", "E322, ES Library", "E122, ES Cafeteria", "R302, Orchestra Room", "PC Commons", "H116, Old Luther", "P230/P231, Jade/Pearl", "P202", "R110, Drama Room", "H117, Makerspace", "H127, Health Room", "R101, Rittman Theatre", "Room #, Costume Room", "MSR?????", "H404", "H403", "P201, New Luther", "H305", "H205", "P106, Phoenix Center Gym", "Classroom next to Jade/Pearl???", "H203", "H204", "H206", "MS and HS classrooms Schools???"
 ]
+
+var images = [#imageLiteral(resourceName: "Wittenberg Hall"), #imageLiteral(resourceName: "PC Lounge")]
 
 var row = 0
 
