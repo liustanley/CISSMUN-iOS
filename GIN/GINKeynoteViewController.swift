d81f45d3ed8c20ee004d1ebeb749d3de2fab8fa6
Stanley Liu <liustan24@gmail.com>
2017-11-14 17:22:38 +0800

    FIXED

--- GIN/GINKeynoteViewController.swift
+++ GIN/GINKeynoteViewController.swift
@@ -1,35 +1,55 @@
 //
-//  GINKeynoteViewController.swift
-//  CISSMUN
+//  SecondViewController.swift
+//  GIN
 //
-//  Created by Stanley Liu on 6/8/17.
+//  Created by Stanley Liu on 5/24/17.
 //  Copyright © 2017 Stanley Liu. All rights reserved.
 //
 
 import UIKit
 
-class GINKeynoteViewController: UIViewController {
-
+class GINKeynoteViewController: UIViewController, UIScrollViewDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        scrollView.delegate = self
+        
+        self.automaticallyAdjustsScrollViewInsets = false
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        
+        let offsetY = scrollView.contentOffset.y
+        
+        if offsetY < 0
+        {
+            imageView.frame.size.height = -offsetY
+        }
+        else
+        {
+            imageView.frame.size.height = imageView.frame.size.height
+        }
     }
-    */
-
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        let newOrigin = CGPoint(x: 0, y: 0)
+        
+        scrollView.contentOffset = newOrigin
+        scrollView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }
+    
 }
+
+
