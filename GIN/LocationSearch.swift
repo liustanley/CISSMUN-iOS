64a32e10edb01f65668156ea2d3098202f789d5c
Tanya Lai <tanyalai@Tanya-Lai.local>
2017-11-22 23:05:59 +0800

    did some formatting and aesthetically pleasing stuff to the specific room pages from the search thing+room sitch

--- GIN/LocationSearch.swift
+++ GIN/LocationSearch.swift
@@ -15,9 +15,6 @@ var row = 0
 
 class LocationSearch: UITableViewController, UISearchResultsUpdating{
 
-    
-    
-    
     var filtered = [Room]()
     var searchController : UISearchController!
     var resultsController = UITableViewController()
