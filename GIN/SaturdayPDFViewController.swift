d3d964b53208782de629dfa118d5d9f7d4f95915
Tanya Lai <tanyalai@Tanya-Lai.local>
2018-01-13 12:39:26 +0800

    Shanghai PDF updated

---
+++ GIN/SaturdayPDFViewController.swift
@@ -0,0 +1,44 @@
+//
+//  SaturdayPDFViewController.swift
+//  CISSMUN
+//
+//  Created by Tanya Lai on 1/13/18.
+//  Copyright © 2018 Stanley Liu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import WebKit
+
+class SaturdayPDFViewController: UIViewController {
+    
+    var stringPassed = ""
+    var webView: WKWebView!
+    
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        view = webView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let pdfTitle = "Saturday Night Excursion"
+        
+        if let url = Bundle.main.url(forResource: pdfTitle, withExtension: "pdf") {
+            let urlRequest = URLRequest(url: url)
+            webView.load(urlRequest as URLRequest)
+            
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
+
+
+
---
+++ GIN/ShanghaiViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ShanghaiViewController.swift
+//  CISSMUN
+//
+//  Created by Tanya Lai on 1/13/18.
+//  Copyright © 2018 Stanley Liu. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+import WebKit
+
+class ShanghaiViewController: UIViewController, UIScrollViewDelegate, UIWebViewDelegate {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    /*
+     @IBAction func openPDF(_ sender: UIButton) {
+     let pdfVC = storyboard?.instantiateViewController(withIdentifier: "pdfviewer") as! SaturdayPDFViewController
+     pdfVC.stringPassed = sender.accessibilityIdentifier!
+     navigationController?.pushViewController(pdfVC, animated: true)
+     
+     }*/
+    
+}
+
+
