4db18e6b970db3655b03b8bd8950a42b1c64d125
Tanya Lai <tanyalai@tanya-lai.local>
2018-01-14 14:57:23 +0800

    Live Updates button+ web page view added to the schedule part of the app

--- GIN/LiveUpdateViewController.swift
+++ GIN/LiveUpdateViewController.swift
@@ -11,13 +11,13 @@ import WebKit
 
 class LiveUpdateViewController: UIViewController {
     
-    @IBOutlet weak var webview: WKWebView!
+    @IBOutlet weak var webview: UIWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let url = URL(string:"https://docs.google.com/spreadsheets/d/1oL2QfSbLGm1bQ7Bm1quooJ63o1PzPTNOhNLcsGVMTdI/edit?ts=5a5a4fad#gid=1126656288")
-        let request = URLRequest(url:url!)
-        webview.load(request)
+        let url1 = URL(string:"https://docs.google.com/spreadsheets/d/1oL2QfSbLGm1bQ7Bm1quooJ63o1PzPTNOhNLcsGVMTdI/edit?ts=5a5a4fad#gid=1126656288")
+        let request = URLRequest(url:url1!)
+        webview.loadRequest(request)
         // Do any additional setup after loading the view, typically from a nib.
     }
     
