2f5d027bfbdf9e19c5bbc938aef90fb64a99e496
Edward <edwardhuang@Edwards-MacBook-Pro.local>
2018-01-15 10:51:19 +0800

    changed to web view (deprecated)

--- GIN/LiveUpdateViewController.swift
+++ GIN/LiveUpdateViewController.swift
@@ -14,13 +14,15 @@ class LiveUpdateViewController: UIViewController {
     @IBOutlet weak var reupdate: UIBarButtonItem!
     
     
-    @IBOutlet weak var webview: WKWebView!
+    @IBOutlet weak var webview: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let url1 = URL(string:"https://docs.google.com/spreadsheets/d/e/2PACX-1vQzmqAdyzfpoehjGQsvA2Xw1Y4tL1g4-2g80pgeQi-31tAU7oAVUnawwbm9c39vVPSZztutYlXSM2sb/pubhtml?gid=1126656288&single=true")
         let request = URLRequest(url:url1!)
-        webview.load(request)
+        //webview.load(request)
+        webview.loadRequest(request)
         // Do any additional setup after loading the view, typically from a nib.
     }
     
